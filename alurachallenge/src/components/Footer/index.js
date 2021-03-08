ee643c35cc1c2b53b556553dd23123f79d355abc
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2021-03-08 14:43:56 -0300

    footer

---
+++ alurachallenge/src/components/Footer/index.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+
+const FooterWrapper = styled.footer`
+  background-color: rgba(91, 50, 255, 0.6);
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  border-radius: 4px; 
+  img {
+    width: 58px;
+    margin-right: 23px;
+  }
+  a {
+    color: white;
+    text-decoration: none;
+    transition: .3s;
+    &:hover,
+    &:focus {
+      opacity: .5;
+    }
+    span {
+      text-decoration: underline;
+      font-weight:550;
+    }
+  }
+`;
+
+export default function Footer(props) {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <FooterWrapper {...props}>
+      <a href="https://www.alura.com.br/">
+        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      </a>
+      <p>
+        Orgulhosamente criado durante
+        {' '}
+        a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          <span>Imersão React da Alura</span>
+        </a>
+      </p>
+    </FooterWrapper>
+  );
+}
