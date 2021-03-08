8e30434d7c7a8f35523c5bba2a465ab1c88afe4a
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2021-03-08 13:32:13 -0300

    desestruturar

--- alurachallenge/pages/index.js
+++ alurachallenge/pages/index.js
@@ -1,77 +1,29 @@
 import styled from 'styled-components'
+import Widget from '../src/components/Widget';
 
 import db from '../db.json';
 
 export const BackgroundImage = styled.div`
   background-image:url(${db.bg});
   flex:1;
-  background-size:auto;
+  background-size:cover;
   background-position:right;
 `;
 
 export const QuizContainer = styled.div`
   width: 100%;
-  max-width:23rem; 
-  padding-top:4rem; 
-  margin: auto 10%;
+  max-width:21rem; 
+  padding-top: 4rem; 
+  margin: auto 15%;
+  align-items: center;
 
-  @media screen and (max-width: 31,25rem) {
+  @media screen and (max-width: 31.25rem) {
     margin:auto;
     padding:1rem;
   }
 `;
 
-export const Widget = styled.div`
 
-  margin-top: 2rem;
-  margin-bottom: 2.5rem;
-  border: 1px solid #333;
-  background-color: #555;
-  border-radius: 4px;
-  overflow: hidden;
-
-  h1, h2, h3 {
-    font-size: 1rem;
-    font-weight: 700;
-    line-height: 1;
-    margin-bottom:0;
-  }
-
-  p {
-    font-size: 1rem;
-    font-weight:400;
-    line-height:1;
-  }
-`;
-
-Widget.Header = styled.header`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 1rem 2rem;
-  background-color: ${({ theme }) => theme.colors.primary};
-
-  *{
-    
-  }
-
-
-`;
-
-Widget.Content = styled.div`
-  padding: 1.5rem 2rem 2rem 2rem;
-  & > *:first-child {
-    margin-top:0;
-  }
-  & > *:last-child {
-    margin-bottom: 0;
-  }
-  ul {
-    list-style: none;
-    padding:0;
-}
-
-`;
 
 export default function Home() {
   return (
@@ -79,17 +31,18 @@ export default function Home() {
      <QuizContainer>
 
       <Widget>
-        <Widget.Content>
-          <h1> Quiz, Filme, terror...</h1>
 
+        <Widget.Header>
+            <h1> Quiz, Filme, terror...</h1>
+        </Widget.Header>
+
+        <Widget.Content>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit....</p>
         </Widget.Content>
       </Widget>
       
       <Widget>
         <Widget.Content>
-          <h1> Quiz dos Amigos </h1>
-
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit....</p>
         </Widget.Content>
       </Widget>
---
+++ alurachallenge/src/components/Widget/index.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+const Widget = styled.div`
+
+  margin-top: 2rem;
+  margin-bottom: 2.5rem;
+  border: 1px solid ${({ theme }) => theme.colors.border};
+  background-color: ${({ theme }) => theme.colors.mainBg};
+  border-radius: 4px;
+  overflow: hidden;
+
+  h1, h2, h3 {
+    font-size: 1rem;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom:0;
+
+    h1 {
+      color: ${({ theme }) => theme.colors.contrastText}
+    }
+    
+  }
+
+  p {
+    font-size: 1rem;
+    font-weight:400;
+    line-height:1;
+  }
+`;
+
+Widget.Header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 1rem 2rem;
+  background-color: ${({ theme }) => theme.colors.primary};
+ 
+  *{
+    margin:0;
+  }
+`;
+
+Widget.Content = styled.div`
+  padding: 1.5rem 2rem 2rem 2rem;
+  align-items: center;
+
+  & > *:first-child {
+    margin-top:0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding:0;
+}
+`;
+export default Widget;
